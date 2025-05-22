a6b50a4eda4b82bbb93f252a1aebd4b45cbfbe98
Mac Anderson <mac@macanderson.com>
2025-05-21 18:14:52 -0700

    Update greeting.tsx

--- components/greeting.tsx
+++ components/greeting.tsx
@@ -13,7 +13,7 @@ export const Greeting = () => {
         transition={{ delay: 0.5 }}
         className="text-2xl font-semibold"
       >
-        Hello there!
+        Hello there! My name is Kizzy.
       </motion.div>
       <motion.div
         initial={{ opacity: 0, y: 10 }}
@@ -22,7 +22,7 @@ export const Greeting = () => {
         transition={{ delay: 0.6 }}
         className="text-2xl text-zinc-500"
       >
-        How can I help you today?
+        I am powered by Kizen AI how can I help you today?
       </motion.div>
     </div>
   );
