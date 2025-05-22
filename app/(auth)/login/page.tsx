813677d03f80b9fe0072f4bc0f428c9e2b033b50
Mac Anderson <mac@macanderson.com>
2025-05-21 18:24:21 -0700

    Update page.tsx

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -55,7 +55,7 @@ export default function Page() {
         <div className="flex flex-col items-center justify-center gap-2 px-4 text-center sm:px-16">
           <h3 className="text-xl font-semibold dark:text-zinc-50">Sign In</h3>
           <p className="text-sm text-gray-500 dark:text-zinc-400">
-            Use your email and password to sign in
+            Sign in to discover why you should hire Mac Anderson.
           </p>
         </div>
         <AuthForm action={handleSubmit} defaultEmail={email}>
